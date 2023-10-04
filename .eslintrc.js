c5345a93ad850ee5c6f0dd6dab26016debaaab62
victortokopedia <victor.irawan@tokopedia.com>
2023-10-04 15:37:32 +0700

    feat: add config

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/require-default-props': 'off',
+    'react/no-unknown-property': ['error', { ignore: ['css'] }],
   },
   settings: {
     'import/resolver': {
--- skipper.config.js
+++ skipper.config.js
@@ -1,3 +1,20 @@
 const define = require('@tokopedia/skipper/cli/config/define-config');
 
-module.exports = define();
+module.exports = define({
+  babel: {
+    server: {
+      plugins: [['@emotion/babel-plugin']],
+      skipperPresetOptions: {
+        importSource: '@emotion/react',
+      },
+    },
+    client: {
+      plugins: [['@emotion/babel-plugin']],
+      skipperPresetOptions: {
+        importSource: '@emotion/react',
+      },
+    },
+  },
+  globalCSS: false,
+  transpilePackages: ['@nest-ui/core', '@nest-ui/icon'],
+});
