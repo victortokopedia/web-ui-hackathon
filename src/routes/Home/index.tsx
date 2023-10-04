da650dd3fba79cbc921e696080772fd7b139f2d3
Andy Wihalim <andy.wihalim@tokopedia.com>
2023-10-04 16:48:16 +0700

    feat(home): improve layout

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Breadcrumbs, Button, Card, Container, Grid, Image } from '@nest-ui/core';
+import { Breadcrumbs, Button, Card, Container, Divider, Grid, Image, Typography } from '@nest-ui/core';
 import { AddSmall } from '@nest-ui/icon';
 
 const Home = () => {
@@ -6,12 +6,12 @@ const Home = () => {
     <>
       <div>
         <Container>
-          <Breadcrumbs overflow={4}>
+          <Breadcrumbs overflow={4} css={{ marginBottom: '1rem' }}>
             <Breadcrumbs.Item href="#">Home</Breadcrumbs.Item>
             <Breadcrumbs.Item href="#">PC & Laptop</Breadcrumbs.Item>
             <Breadcrumbs.Item href="#">Laptop</Breadcrumbs.Item>
             <Breadcrumbs.Item href="#">Consumer Laptop</Breadcrumbs.Item>
-            <Breadcrumbs.Item as="h1">Apple Macbook Pro 2021 M1 Max 16 512GB</Breadcrumbs.Item>
+            <Breadcrumbs.Item>Apple Macbook Pro 2021 M1 Max 16 512GB</Breadcrumbs.Item>
           </Breadcrumbs>
           <Grid gutterX={40}>
             <Grid.Col>
@@ -22,23 +22,36 @@ const Home = () => {
                   </div>
                 </Grid.Col>
                 <Grid.Col>
-                  <h1>Apple Macbook Pro 2021 M1 Max 16 512GB</h1>
+                  <Typography as="h1" variant="heading6">
+                    Apple Macbook Pro 2021 M1 Max 16 512GB
+                  </Typography>
                   <Card css={{ padding: '1rem' }}>Product Detail</Card>
                 </Grid.Col>
               </Grid>
+              <Divider css={{ margin: '1rem 0 4rem' }} />
               <section>
-                <h2>ULASAN PEMBELI</h2>
+                <Typography as="h2" variant="display1" css={{ margin: '0 0 1.25rem' }}>
+                  <strong>ULASAN PEMBELI</strong>
+                </Typography>
                 <Card css={{ padding: '1rem' }}>Content</Card>
               </section>
               <section>
-                <h2>Diskusi</h2>
+                <Typography as="h2" variant="Display1">
+                  <strong>Diskusi (2)</strong>
+                  <Typography as="span" variant="paragraph2" css={{ display: 'block', marginTop: '.5rem' }}>
+                    Apple Macbook Pro 2021 M1 Max 16 512GB
+                  </Typography>
+                </Typography>
                 <Card css={{ padding: '1rem' }}>Content</Card>
               </section>
+              <Divider css={{ margin: '1rem 0 0' }} />
             </Grid.Col>
             <Grid.Col span="auto">
               <div css={{ width: 268, position: 'sticky', top: 0 }}>
                 <Card css={{ padding: '1rem' }}>
-                  <p>Atur jumlah dan catatan</p>
+                  <Typography as="h3" variant="display1" css={{ margin: '0 0 1.25rem' }}>
+                    <strong>Atur jumlah dan catatan</strong>
+                  </Typography>
                   <Button block icon={<AddSmall aria-label="Tambah" />} css={{ marginBottom: '.5rem' }}>
                     Keranjang
                   </Button>
@@ -53,13 +66,17 @@ const Home = () => {
       </div>
       <section>
         <Container>
-          <h2>Lainnya di toko ini</h2>
+          <Typography as="h2" variant="heading4">
+            Lainnya di toko ini
+          </Typography>
           <Card css={{ padding: '1rem' }}>Content</Card>
         </Container>
       </section>
       <section>
         <Container>
-          <h2>Produk sponsor terkait</h2>
+          <Typography as="h2" variant="heading4">
+            Produk sponsor terkait
+          </Typography>
           <Card css={{ padding: '1rem' }}>Content</Card>
         </Container>
       </section>
