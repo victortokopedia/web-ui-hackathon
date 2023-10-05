fb7c048595171440d0d7e7deb6d9b6dec6fb754e
victortokopedia <victor.irawan@tokopedia.com>
2023-10-05 10:04:49 +0700

    fix: qrmodal

--- src/routes/Home/components/PurchaseInApp/styles.ts
+++ src/routes/Home/components/PurchaseInApp/styles.ts
@@ -1,5 +1,5 @@
 import { css } from '@emotion/react';
-import { NN0, NN600, NN900 } from '@tokopedia/nest-color';
+import { NN0, NN600, NN900, addAlpha } from '@tokopedia/nest-color';
 
 export const modalOverlayWrapper = css`
   width: 100vw;
@@ -14,18 +14,39 @@ export const modalOverlayWrapper = css`
 `;
 
 export const modalQRWrapper = css`
-  display: none;
-  position: absolute;
   width: 632px;
   height: 470px;
   margin: auto;
-  z-index: 1000;
   background-color: ${NN0};
-  top: 0;
-  bottom: 0;
-  right: 0;
-  left: 0;
   padding: 24px 16px;
+  border: none;
+  border-radius: 0.625rem;
+  &::backdrop {
+    background: ${addAlpha(NN900, 0.5)};
+    opacity: 0;
+    transition-property: opacity display;
+    transition: 0.25s ease-out;
+    transition-behavior: allow-discrete;
+  }
+  transform: scale(0.8);
+  opacity: 0;
+  transition-property: transform opacity display;
+  transition: 0.25s ease-out;
+  transition-behavior: allow-discrete;
+  &:popover-open {
+    @starting-style {
+      transform: scale(0.8);
+      opacity: 0;
+      &::backdrop {
+        opacity: 0;
+      }
+    }
+    transform: scale(1);
+    opacity: 1;
+    &::backdrop {
+      opacity: 1;
+    }
+  }
 `;
 
 export const buttonClose = css`
